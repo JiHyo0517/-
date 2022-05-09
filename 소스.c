25cb47481219970b8b582c8ba5f130a343253ee5
김지효 <@DESKTOP-C3TKVHT>
2022-05-09 10:42:49 +0900

    초기 커밋.

---
+++ 소스.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_STACK_SIZE 5
+
+typedef char element;
+typedef struct {
+	element data[MAX_STACK_SIZE];
+	int top;
+} StackType;
+
+// 스택 초기화 함수
+void init_stack(StackType* s) {
+	s->top = -1;
+}
+
+// 공백 상태 검출 함수
+int is_empty(StackType* s) {
+	return (s->top == -1);
+}
+// 포화 상태 검출 함수
+int is_full(StackType* s) {
+	return (s->top == (MAX_STACK_SIZE - 1));
+}
+// 삽입함수
+void push(StackType* s, element item) {
+	if (is_full(s)) {
+		fprintf(stderr, "스택 포화 에러\n");
+		return;
+	}
+	else s->data[++(s->top)] = item;
+}
+// 삭제함수
+element pop(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else return s->data[(s->top)--];
+}
+int check_matching(const char* in) {
+	StackType s;
+	char ch, open_ch;
+	int i, n = strlen(in);//string.h
+	
+	init_stack(&s);
+
+	for (i = 0; i < n; i++) {
+		ch = in[i]; // {
+		switch(ch) {
+		case '{': case'[': case '(':
+			push(&s, ch); break;
+		case '}': case']': case ')':
+			if (is_empty(&s)) return 0;
+			else {
+				open_ch = pop(&s);
+				if ((open_ch == '(' && ch != ')') ||
+					(open_ch == '{' && ch != '}') ||
+					(open_ch == '[' && ch != ']'))
+					return 0;
+			}
+			break;
+		}
+	}	// for end
+	if (!is_empty(&s)) return 0;
+	return 1;
+}
+int main() {
+	const char* p = "{A[(i+1)]=0;}";
+
+	if (check_matching(p) == 1)
+		printf("%s: 괄호 검사 성공\n", p);
+	else
+		printf("%s: 괄호 검사 실패\n", p);
+}
